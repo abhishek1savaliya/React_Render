44ff5c36d9660d33134bf3f5f8a369577b5172f4
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-06-06 17:13:44 +0530

    seMemo and useCallback

--- src/Components/IncorrectOptimization/ParentFour.js
+++ src/Components/IncorrectOptimization/ParentFour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useMemo,useCallback } from 'react';
 import { MemorizedChildFive } from './ChildFive';
 
 const ParentFour = () => {
@@ -9,17 +9,21 @@ const ParentFour = () => {
         fname  : 'Jagannath',
         lname : 'Mohanam'
     } 
+    // const memorizePerson = useMemo(()=>person,[])
+    
 
     console.log('ParentFour Render');
 
     const handleClick = ()=>{ }
+    const memorizeHandleClick = useCallback(handleClick, [])
 
     return (
         <div>
             <button onClick={() => setCount(c => c + 1)}>Count - {count}</button>
             <button onClick={() => setName('Radha')}>Change Name</button>
             <div>
-                <MemorizedChildFive name={name} handleClick={handleClick} />
+                {/* <MemorizedChildFive name={name} person={memorizePerson} /> */}
+                <MemorizedChildFive name={name} handleClick={memorizeHandleClick} />
                 {/* <ChildThree name={name}>
                     <strong>Hello</strong></ChildThree> */}
             </div>
